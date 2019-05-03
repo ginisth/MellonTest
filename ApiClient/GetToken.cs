868e84cc07a2987f46d15e53f47df74e032fbed1
ginisth <ginisth@hotmail.com>
2019-05-03 14:28:02 +0300

    Initialize project

---
+++ ApiClient/GetToken.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    public class GetToken
+    {
+        public string Token { get; set; }
+
+        public async Task<string> CatchToken(string username, string password)
+        {
+            // Prevent SSL exceptions
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = (senderX, certificate, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient();
+            var byteArray = Encoding.ASCII.GetBytes(username + ":" + password);
+
+            client.BaseAddress = new Uri("https://34.240.190.150/api/api-token-auth/");
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            var body = new List<KeyValuePair<string, string>>
+            {
+                    new KeyValuePair<string, string>("grant_type", "password"),
+                    new KeyValuePair<string, string>("username", username),
+                    new KeyValuePair<string, string>("password", password)
+            };
+
+            var content = new FormUrlEncodedContent(body);
+
+            try
+            {
+                HttpResponseMessage response = await
+                    client.PostAsync("https://34.240.190.150/api/api-token-auth/", content);
+
+                response.EnsureSuccessStatusCode();
+                string token = null;
+                if (response.IsSuccessStatusCode)
+                    token = await response.Content.ReadAsStringAsync();
+
+                GetToken desirializedToken = Newtonsoft.Json.JsonConvert.DeserializeObject<GetToken>(token);
+
+                return desirializedToken.Token;
+            }
+            catch (HttpRequestException e)
+            {
+                return e.Message.ToString();
+            }
+        }
+    }
+}
--- MellonTest/Program.cs
+++ MellonTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApiClient;
 
 namespace MellonTest
 {
@@ -10,12 +11,18 @@ namespace MellonTest
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            var token = new GetToken().CatchToken("TestUser", "ZfuzpbZ8Mo4").GetAwaiter().GetResult();
+            Console.WriteLine($"Token:{token}");
+            var actors = new Actors(token).GetActors().GetAwaiter().GetResult();
+            foreach (var actor in actors)
+            {
+                Console.WriteLine(actor.Id + " " + actor.Name + " " + actor.Birthday);
+            }
+            Console.WriteLine(actors);
+            //
+            //
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
